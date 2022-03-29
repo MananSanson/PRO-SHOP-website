3c5c040adcaa13445df1102683c5465a7a83ba41
Manan <manansanson22@gmail.com>
2022-03-29 17:26:04 +0530

    completed module 3

---
+++ src/products.js
@@ -0,0 +1,78 @@
+const products=[
+    {
+        _id: "1",
+        name: "Alexa smart Speaker",
+        image: "/images/alexa.jpg",
+        description:
+        "Alexa is the intelligent cloud-based voice AI that you can talk to on [your product]. Speak to Alexa through [your product] to play music, hear the news, check weather, control smart home devices, and more.",
+        brand: "Amazon",
+        category:"Electronics",
+        price: 3000,
+        countInStock: 3,
+        rating: 4.5,
+        numsReview: 14,
+    },
+    { 
+    _id: "2",
+    name: "Lenevo Laptop Bag",
+    image: "/images/bag.jpg",
+    description:
+    " A laptop sleeve, also sometimes called a laptop pouch or bag, is typically a sleeve/bag/pouch manufactured from a material such as neoprene or leather, that is slightly larger than the laptop, and designed to protect the device from bumps, scratches, water and dust in everyday use.",
+    brand: "Lenevo",
+    category:"Accessories",
+    price: 1500,
+    countInStock: 0,
+    rating: 1.5,
+    numsReview: 25,
+    },
+
+    {
+        _id:"3",
+        name:"Samsung Buds",
+        image:"/images/buds.jpg",
+        description:
+        "Our next-generation adaptive dual microphone technology makes background",
+        brand: "Samsung",
+        category:"Accessories",
+        price:10000,
+        countInStock:3,
+        rating:3,
+        numsReview:114,
+    },
+    {
+        _id: "4",
+        name:"iphone 13 pro",
+        image:"/images/iphone.jpg",
+        description:"The iPhone is a smartphone made by Apple that combines a computer, iPod, digital camera and cellular phone into one device with a touchscreen interface",
+        brand: "Apple",
+        category: "Electronics",
+        price: 130000,
+        countInStock:1,
+        rating: 4,
+        numsReview: 55,
+    },
+    {
+        _id: "5",
+        name:"pixel 6 pro",
+        image:"/images/pixel.jpg",
+        description:"easy to use Fantastic camera and camera software Reasonable price for a flagship phone",
+        brand: "Google",
+        price: 65000,
+        countInStock: 2,
+        rating: 3.5,
+        numsReview:34,
+    },
+    {
+        _id: "6",
+        name:"256GB Pendrive",
+        image:"/images/pendrive.jpg",
+        description:"SanDisk Ultra Dual Drive Go Type C Pendrive for Mobile 256GB ",
+        brand: "ScanDisk",
+        price: 2500,
+        countInStock: 2,
+        rating: 2.5,
+        numsReview:4,
+    }
+]
+
+export default products;
